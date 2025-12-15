10bcc5e96a0921fceee5bdba213e5ef83cc18a7c
ashu1049 <ashutoshthorat2@gmail.com>
2025-12-15 15:45:20 +0530

    15/12

---
+++ Collections/Arraylist1.java
@@ -0,0 +1,15 @@
+package Collections;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Arraylist1{
+    
+    public static void main(String[] args) {
+        
+        List<Integer> list = new ArrayList<>();
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        System.out.println(list);
+    }
+}
