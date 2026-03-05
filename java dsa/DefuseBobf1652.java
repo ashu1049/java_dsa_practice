6c31a7159e3f6c703f38a583bd823c38c5b91f9b
ashu1049 <ashutoshthorat2@gmail.com>
2026-03-05 15:53:19 +0530

    update

---
+++ java dsa/DefuseBobf1652.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int[] decrypt(int[] code, int k) {
+        int n=code.length;
+        int[] arr= new int[n];
+
+        return arr;
+    }
+}
+
+public class DefuseBobf1652
+{
+    public static void main(String[] args) {
+        int arr[]={5,7,1,4};
+        int k=3;
+
+        System.out.println(Solution.decrypt(arr, k));
+    }
+}
---
+++ java dsa/LinkedListCycleTest.java
@@ -0,0 +1,53 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+public class LinkedListCycleTest {
+    public static void main(String[] args) {
+
+        // Creating nodes
+        ListNode head = new ListNode(3);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(0);
+        ListNode fourth = new ListNode(-4);
+
+        // Connecting nodes
+        head.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        // Creating a cycle: last node points back to second node
+        fourth.next = second;
+
+        Solution obj = new Solution();
+        boolean result = obj.hasCycle(head);
+
+        System.out.println("Does the linked list have a cycle? " + result);
+    }
+}
